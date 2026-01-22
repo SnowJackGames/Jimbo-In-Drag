a8fc5c61b299d678a6340f02e49aa51bd2c94a49
KassLavender <54489915+KassLavender@users.noreply.github.com>
2026-01-21 19:12:20 -0700

    tarot placeholder

--- content/atlas.lua
+++ content/atlas.lua
@@ -1,10 +1,10 @@
 -- Registers the atlases for:
     -- Jokers
     -- Suits
-SMODS.Atlas { key = 'lc_cards', path = 'Suits/8BitDeck.png', px = 71, py = 95 }
-SMODS.Atlas { key = 'hc_cards', path = 'Suits/8BitDeck_opt2.png', px = 71, py = 95 }
-SMODS.Atlas { key = 'lc_ui', path = 'Suits/ui_assets.png', px = 18, py = 18 }
-SMODS.Atlas { key = 'hc_ui', path = 'Suits/ui_assets_opt2.png', px = 18, py = 18 }
+SMODS.Atlas { key = "lc_cards", path = "Suits/8BitDeck.png", px = 71, py = 95 }
+SMODS.Atlas { key = "hc_cards", path = "Suits/8BitDeck_opt2.png", px = 71, py = 95 }
+SMODS.Atlas { key = "lc_ui", path = "Suits/ui_assets.png", px = 18, py = 18 }
+SMODS.Atlas { key = "hc_ui", path = "Suits/ui_assets_opt2.png", px = 18, py = 18 }
     -- Ranks
     -- Decks
     -- Blinds
@@ -17,6 +17,7 @@ SMODS.Atlas { key = 'hc_ui', path = 'Suits/ui_assets_opt2.png', px = 18, py = 18
     -- Consumables
         -- Planets
         -- Tarot
+SMODS.Atlas { key = "Tarot", path = "Consumables/Tarot.png", px = 71, py = 95 }
         -- Spectrals
         -- Vouchers
 		-- Packs
