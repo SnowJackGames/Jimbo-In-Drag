3d0e0a5279d4f915833e500c701aa8626f654fe5
KassLavender <54489915+KassLavender@users.noreply.github.com>
2026-02-02 18:18:26 -0700

    testing a thing with build_backup_en_us_localization

--- utilities/late_code.lua
+++ utilities/late_code.lua
@@ -22,3 +22,80 @@ function DRAGQUEENMOD.build_custom_structure_dictionary_tooltips()
     end
   end
 end
+
+
+
+-- If a localization entry sought from `DRAGQUEENMOD.easydescriptionlocalize` or `DRAGQUEENMOD.easymisclocalize`
+-- <br>doesn't exist in the current language, we try to find the entry in en-us as a backup.
+function DRAGQUEENMOD.build_backup_en_us_localization()
+  local backup_en_us_localization = assert(loadstring(love.filesystem.read("localization/en-us.lua")))()
+
+  -- The below code is basically Balatro's `functions/misc-functions.lua:init_localization()` copy-pasted
+  backup_en_us_localization.misc.v_dictionary_parsed = {}
+  for k, v in pairs(backup_en_us_localization.misc.v_dictionary) do
+    if type(v) == 'table' then
+      backup_en_us_localization.misc.v_dictionary_parsed[k] = {multi_line = true}
+      for kk, vv in ipairs(v) do
+        backup_en_us_localization.misc.v_dictionary_parsed[k][kk] = loc_parse_string(vv)
+      end
+    else
+      backup_en_us_localization.misc.v_dictionary_parsed[k] = loc_parse_string(v)
+    end
+  end
+  backup_en_us_localization.misc.v_text_parsed = {}
+  for k, v in pairs(backup_en_us_localization.misc.v_text) do
+    backup_en_us_localization.misc.v_text_parsed[k] = {}
+    for kk, vv in ipairs(v) do
+      backup_en_us_localization.misc.v_text_parsed[k][kk] = loc_parse_string(vv)
+    end
+  end
+  backup_en_us_localization.tutorial_parsed = {}
+  for k, v in pairs(backup_en_us_localization.misc.tutorial) do
+    backup_en_us_localization.tutorial_parsed[k] = {multi_line = true}
+      for kk, vv in ipairs(v) do
+        backup_en_us_localization.tutorial_parsed[k][kk] = loc_parse_string(vv)
+      end
+  end
+  backup_en_us_localization.quips_parsed = {}
+  for k, v in pairs(backup_en_us_localization.misc.quips or {}) do
+    backup_en_us_localization.quips_parsed[k] = {multi_line = true}
+      for kk, vv in ipairs(v) do
+        backup_en_us_localization.quips_parsed[k][kk] = loc_parse_string(vv)
+      end
+  end
+  for g_k, group in pairs(backup_en_us_localization) do
+    if g_k == 'descriptions' then
+      for _, set in pairs(group) do
+        for _, center in pairs(set) do
+          center.text_parsed = {}
+          if not center.text then else
+            for _, line in ipairs(center.text) do
+                if type(line) == 'table' then
+                    center.text_parsed[#center.text_parsed+1] = {}
+                    for _, new_line in ipairs(line) do
+                        center.text_parsed[#center.text_parsed][#center.text_parsed[#center.text_parsed]+1] = loc_parse_string(new_line)
+                    end
+                else
+                    center.text_parsed[#center.text_parsed+1] = loc_parse_string(line)
+                end
+            end
+            center.name_parsed = {}
+            for _, line in ipairs(type(center.name) == 'table' and center.name or {center.name}) do
+              center.name_parsed[#center.name_parsed+1] = loc_parse_string(line)
+            end
+            if center.unlock then
+              center.unlock_parsed = {}
+              for _, line in ipairs(center.unlock) do
+                center.unlock_parsed[#center.unlock_parsed+1] = loc_parse_string(line)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  DRAGQUEENMOD.backup_en_us_localization = backup_en_us_localization
+
+  print(backup_en_us_localization)
+end
