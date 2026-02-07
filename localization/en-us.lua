42c2ead23d700f251b25dc431c2d8873bd59b420
KassLavender <54489915+KassLavender@users.noreply.github.com>
2026-02-06 22:28:14 -0700

    dragqueen_dictionary_accessorize verbage

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -621,7 +621,7 @@ return {
         text = {
           "{s:0.9}When this item is obtained, create{}",
           "{s:0.9}up to {}{C:attention,s:0.9}X{}{s:0.9} number of the{}",
-          "{s:0.9}associated {}{C:dragqueen_consumable,s:0.9}Consumable{}{s:0.9} on the{}",
+          "{s:0.9}associated {}{C:attention,s:0.9}consumable{}{s:0.9} on the{}",
           "{s:0.9}{}{C:attention,s:0.9}\"Suit To Consumable Table\"{}",
           "{s:0.9}for that {}{C:attention,s:0.9}suit{}{s:0.9} if you have room; i.e.{}",
           "{s:0.9}\"{}{C:white,X:dragqueen_keyword,s:0.9}Accessorize{}{s:0.9} {}{C:hearts,s:0.9}Hearts{}{s:0.9} {}{C:attention,s:0.9}1{}{s:0.9}\" creates{}",
