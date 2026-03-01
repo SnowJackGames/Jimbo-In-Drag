c034f20f272061f8068eae54bb9b47cb89695ade
KassLavender <54489915+KassLavender@users.noreply.github.com>
2026-02-28 22:31:50 -0700

    fix to force strength and strength-like effects to change suits how we want

--- content/Ranks/mother.lua
+++ content/Ranks/mother.lua
@@ -10,33 +10,36 @@
 -- Straight Specflush House, Straight Specflush Five
 
 -- suitmap fallback code courtesy of Mintys Silly Mod
-local suitmap = {
-  Fallback = 0,
-  dragqueen_Purses = 1,
-  dragqueen_Pumps = 2,
-  Hearts = 3,
-  Clubs = 4,
-  Diamonds = 5,
-  Spades = 6,
-  paperback_Stars = 7,
-  paperback_Crowns = 8,
-  bunc_Fleurons = 9,
-  bunc_Halberds = 10,
-  minty_3s = 9
-}
 
-if SMODS.Suits then
-  for suit, data in pairs(SMODS.Suits) do
-    if not suitmap[suit] and not data.dragqueen_motherrank then
-      suitmap[suit] = 0
+function DRAGQUEENMOD.mother_suit_map_set_up()
+  DRAGQUEENMOD.mother_suitmap = {
+    Fallback = 0,
+    dragqueen_Purses = 1,
+    dragqueen_Pumps = 2,
+    Hearts = 3,
+    Clubs = 4,
+    Diamonds = 5,
+    Spades = 6,
+    paperback_Stars = 7,
+    paperback_Crowns = 8,
+    bunc_Fleurons = 9,
+    bunc_Halberds = 10,
+    minty_3s = 9
+  }
+
+  if SMODS.Suits then
+    for suit, data in pairs(SMODS.Suits) do
+      if not DRAGQUEENMOD.mother_suitmap[suit] and not data.dragqueen_motherrank then
+        DRAGQUEENMOD.mother_suitmap[suit] = 0
+      end
     end
+  else
+    sendDebugMessage("Error defining suit map for Mothers; " ..
+    "SMODS.Suits doesn't seem to be defined at this time for some reason", "Jimbo in Drag")
   end
-else
-  sendDebugMessage("Error defining suit map for Mothers; " ..
-  "SMODS.Suits doesn't seem to be defined at this time for some reason", "Jimbo in Drag")
 end
 
-
+DRAGQUEENMOD.mother_suit_map_set_up()
 
 SMODS.Rank {
   key = "Mother",
@@ -53,10 +56,13 @@ SMODS.Rank {
   strength_effect = {
     fixed = 14
   },
+  prev_behavior = {
+    fixed = 10
+  },
   nominal = 10,
   face = true,
 
-  suit_map = suitmap,
+  suit_map = DRAGQUEENMOD.mother_suitmap,
 
   in_pool = function(self, args)
     -- Allows creating Mothers at the start of a run, like with a deck or sleeve
@@ -72,3 +78,72 @@ SMODS.Rank {
     return false
   end
 }
+
+------------------------------
+-- Vanilla fixes
+------------------------------
+
+-- Fixes so that if SMODS.modify_rank changes a card's rank, relationship with regards to Mothers work as intended
+
+---@diagnostic disable: assign-type-mismatch
+-- I don't know why strength_effect (and its equivalent prev_behavior) in SMODS.modify_rank works like this,
+-- But entries in rank_data.prev and rank_data.next are strings like "King" etc
+-- but strength_effect / prev_behavior requires being an integer. And fixed behavior only pulls
+-- index 0 of .prev and .next.
+-- See the patch in suits_and_ranks.toml, and
+---@see SMODS.modify_rank
+
+SMODS.Rank:take_ownership("Ace",
+  {
+    strength_effect = {
+      fixed = "2"
+    },
+    prev_behavior = {
+      fixed = "King"
+    }
+  }
+)
+
+SMODS.Rank:take_ownership("King",
+  {
+    strength_effect = {
+      fixed = "Ace"
+    },
+    prev_behavior = {
+      fixed = "Queen"
+    }
+  }
+)
+
+SMODS.Rank:take_ownership("Queen",
+  {
+    strength_effect = {
+      fixed = "King"
+    },
+    prev_behavior = {
+      fixed = "Jack"
+    }
+  }
+)
+
+SMODS.Rank:take_ownership("Jack",
+  {
+    strength_effect = {
+      fixed = "Queen"
+    },
+    prev_behavior = {
+      fixed = "10"
+    }
+  }
+)
+
+SMODS.Rank:take_ownership("10",
+  {
+    strength_effect = {
+      fixed = "Jack"
+    },
+    prev_behavior = {
+      fixed = "9"
+    }
+  }
+)
